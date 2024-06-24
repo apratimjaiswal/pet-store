890757e20466276f1f4ee4275acfc5e97f3623a6
Apratim Jaiswal <apratim99@gmail.com>
2024-06-24 19:50:51 +0530

    Initial commit

---
+++ src/main/java/com/playground/pet_store/MyPetStoreApplication.java
@@ -0,0 +1,13 @@
+package com.playground.pet_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPetStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyPetStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/playground/pet_store/MyPetStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.playground.pet_store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPetStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
